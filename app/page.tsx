14facaeb8f08d7ae03cf4c9215bf4e7c7dc1be3e
ghazwanz <ababilghazwan@gmail.com>
2025-07-23 10:37:16 +0700

    update nav guest

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { EnvVarWarning } from '@/components/env-var-warning';
 import { ThemeSwitcher } from '@/components/theme-switcher';
 import { hasEnvVars } from '@/lib/utils';
 import { QRForm } from '@/components/QRForm';
+import { AuthButton } from '@/components/auth-button';
 
 export default async function Home() {
   const supabase = await createClient();
@@ -22,7 +23,7 @@ export default async function Home() {
         {/* NAVBAR */}
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
-            <Link href="/protected">
+            <Link href="/">
               <div className="flex items-center gap-3">
                 <Image
                   src="/logo1.png"
@@ -39,25 +40,14 @@ export default async function Home() {
             {!hasEnvVars ? (
               <EnvVarWarning />
             ) : (
-              <div className="space-x-2 flex">
+              <div className="space-x-5 flex justify-center items-center">
                 <Link
-                  href="/login"
-                  className="px-6 py-2.5 rounded-xl font-medium bg-neutral-200 text-neutral-900 hover:bg-blue-600 hover:text-white dark:bg-neutral-800 dark:text-white dark:hover:bg-blue-500 transition"
-                >
-                  Sign In
-                </Link>
-                <Link
-                  href="/register"
-                  className="px-6 py-2.5 rounded-xl font-medium bg-neutral-100 text-neutral-900 hover:bg-emerald-600 hover:text-white dark:bg-neutral-700 dark:text-white dark:hover:bg-emerald-500 transition"
-                >
-                  Sign Up
-                </Link>
-                 <Link
                   href="/scan"
-                  className="px-6 py-2.5 rounded-xl font-medium bg-neutral-100 text-neutral-900 hover:bg-emerald-600 hover:text-white dark:bg-neutral-700 dark:text-white dark:hover:bg-emerald-500 transition"
+                  className="font-medium text-neutral-500 hover:text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300 transition"
                 >
                   Scan
                 </Link>
+                <AuthButton />
               </div>
             )}
           </div>
