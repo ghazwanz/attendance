0a31edaa5265a770b6a4722a6ceb8b57f0334dba
ghazwanz <ababilghazwan@gmail.com>
2025-07-26 15:18:09 +0700

    up ui jadwal page

--- app/protected/schedule/page.tsx
+++ app/protected/schedule/page.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import Tabeljadwal from './tabeljadwal';
-import Tambahjadwal from './Tambahjadwal';
+// import Tambahjadwal from './Tambahjadwal';
 
-const page = async () => {
+const page = () => {
     
     return (
         <div className="rounded-2xl shadow-lg dark:shadow-white/20 p-8">
-            <div className="mb-6 flex items-center justify-between">
+            <div className="mb-6 flex items-center justify-center text-center">
                 <div>
                     <h2 className="text-3xl font-bold">📋 Tabel Jadwal</h2>
                     <p className="text-gray-500 mt-1">Data jadwal kantor secara keseluruhan</p>
                 </div>
-                <Tambahjadwal/>
+                {/* <Tambahjadwal/> */}
             </div>
             <div className="overflow-x-auto rounded-lg border border-gray-200">
                 <Tabeljadwal />
