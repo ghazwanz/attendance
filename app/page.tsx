56ab4c04639f9c9a0239a426a4e5f4ab6f504e65
Aksay1 <kautsaraksay424@gmail.com>
2025-08-05 09:21:02 +0700

    pindah letak location

--- app/page.tsx
+++ app/page.tsx
@@ -111,8 +111,7 @@ export default async function Home() {
             </div>
           </section>
 
-          {/* LOKASI PENGGUNA */}
-          <UserLocationSection />
+          
         </div>
 
         {/* FOOTER */}
