4704add61863dece93aa50172c13857d50e22510
ghazwanz <ababilghazwan@gmail.com>
2025-08-13 15:39:38 +0900

    dev feat: pwa install

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { Geist } from "next/font/google";
 import { ThemeProvider } from "next-themes";
 import "./globals.css";
@@ -8,10 +8,49 @@ const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
   : "http://localhost:3000";
 
+const APP_NAME = "SiAbsen App";
+const APP_DEFAULT_TITLE = "Sistem Absensi – Berbasis QR & Real-Time Tracking";
+const APP_TITLE_TEMPLATE = "%s - SiAbsen App";
+const APP_DESCRIPTION = "Permudah absensi karyawan dengan sistem berbasis QR.";
 export const metadata: Metadata = {
+  applicationName: APP_NAME,
   metadataBase: new URL(defaultUrl),
-  title: "Sistem Absensi – Berbasis QR & Real-Time Tracking",
-  description: "Permudah absensi karyawan dengan sistem berbasis QR. Pantau kehadiran real-time, kelola izin, dan buat laporan secara cepat dalam satu platform.",
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE
+  },
+  description: APP_DESCRIPTION,
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 const geistSans = Geist({
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const withPWA = require("@ducanh2912/next-pwa").default({
+  dest: "public",
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
+};
+
+module.exports = {
+  experimental: {
+    serverActions: {
+      allowedOrigins: [
+        'localhost:3000', // localhost
+        'ghbjp298-3000.asse.devtunnels.ms/', // Codespaces
+      ],
+    },
+  },
 };
 
 export default nextConfig;
