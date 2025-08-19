dfe510d799bc1480a90e85e72877e9fae843cb92
ghazwanz <ababilghazwan@gmail.com>
2025-08-19 09:05:33 +0700

    fix user page

---
+++ app/protected/users/components/ActionButtons.tsx
@@ -0,0 +1,64 @@
+import { User } from "@/lib/type";
+
+export type ActionButtonsProps = {
+    user:User;
+    currentUser:User;
+    isPending: boolean;
+    setSelectedUser: (user: User) => void;
+    setEditModalOpen: (open: boolean) => void;
+    setUpdatePassModal: (open: boolean) => void;
+    setUserToDelete: (user: User) => void;
+    setDeleteModalOpen: (open: boolean) => void;
+}
+
+export default function ActionButtons({
+    user,
+    currentUser,
+    isPending,
+    setSelectedUser,
+    setEditModalOpen,
+    setUpdatePassModal,
+    setUserToDelete,
+    setDeleteModalOpen,
+}: ActionButtonsProps) {
+    return (
+        <>
+            <button
+                onClick={(e) => {
+                    e.stopPropagation();
+                    setSelectedUser(user);
+                    setEditModalOpen(true);
+                }}
+                className="inline-flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded-full text-xs font-semibold shadow"
+                disabled={isPending}
+            >
+                ✏️ Edit
+            </button>
+            {user.id === currentUser.id && (
+                <button
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setUpdatePassModal(false);
+                    }}
+                    className="inline-flex items-center gap-1 bg-blue-600 hover:bg-blue-700 text-white px-3 py-1 rounded-full text-xs font-semibold shadow"
+                    disabled={isPending}
+                >
+                    ✏️ Ubah Password
+                </button>
+            )}
+            {currentUser?.role === "admin" && (
+                <button
+                    onClick={(e) => {
+                        e.stopPropagation();
+                        setUserToDelete(user);
+                        setDeleteModalOpen(true);
+                    }}
+                    className="inline-flex items-center gap-1 bg-red-600 hover:bg-red-700 text-white px-3 py-1 rounded-full text-xs font-semibold shadow"
+                    disabled={isPending}
+                >
+                    🗑 Hapus
+                </button>
+            )}
+        </>
+    );
+}
