a6fe54a6703ff151e81135dd713dc5bce9d4d05e
ghazwanz <ababilghazwan@gmail.com>
2025-08-20 16:19:16 +0700

    update redirect

--- app/protected/settings/actions/handleEdit.ts
+++ app/protected/settings/actions/handleEdit.ts
@@ -59,7 +59,7 @@ export async function handleEditEmail(
     const { error } = await supabase.auth.updateUser({
         email:email,
     },{
-        emailRedirectTo: `protected`
+        emailRedirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/`
     });
 
     if (error) {
